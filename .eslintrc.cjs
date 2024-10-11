049af97c1db441804ef032dcc9cba587adf7077e
leaglpeogramer <181525998+Administrators-0001@users.noreply.github.com>
2024-10-11 03:04:45 +0300

    Initialize walshy/operation-lint-code

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	env: {
+		browser: true,
+		es2023: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		// Main rules
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'always-multiline',
+			},
+		],
+		'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 } ],
+		'max-len': ['error', { code: 140, tabWidth: 2 }],
+
+		// Disabled due to them being code examples and being far too common
+		'no-undef': ['off'],
+		'@typescript-eslint/no-unused-vars': ['off'],
+	},
+};
---
+++ content/images/cloudflare-images/transform/_index.md
@@ -0,0 +1,11 @@
+---
+pcx_content_type: navigation
+title: Transform images
+weight: 5
+---
+
+# Transform images
+
+Refer to the following edit features for Cloudflare Images.
+
+{{<directory-listing>}}
