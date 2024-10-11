f56288044ed94a1d7cbd75c305989580acb1cf77
leaglpeogramer <181525998+Administrators-0001@users.noreply.github.com>
2024-10-11 04:29:28 +0300

    Initialize docs/workerd

---
+++ assets/main.ts
@@ -0,0 +1,26 @@
+import * as events from "./events";
+import * as contents from "./contents";
+import * as timeago from "./timeago";
+import * as navs from "./navlinks";
+
+declare global {
+  interface Window {
+    docsearch: any;
+    Coveo?: any;
+    zaraz: any;
+  }
+}
+
+(function () {
+  navs.init();
+  timeago.init();
+  events.load();
+  events.focus();
+  events.mobile();
+  events.dropdowns();
+  contents.toc();
+  events.toggleSidebar();
+  events.activeTab();
+  events.tabs();
+  events.zarazTrackDocEvents();
+})();
---
+++ content/fundamentals/api/_index.md
@@ -0,0 +1,9 @@
+---
+title: Cloudflare's API
+weight: 6
+pcx_content_type: overview
+---
+
+# Cloudflare's API
+
+{{<directory-listing>}}
