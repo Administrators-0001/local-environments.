cb82b366095d80deafb29e3770a4da98430aaf8d
leaglpeogramer <181525998+Administrators-0001@users.noreply.github.com>
2024-10-11 03:00:40 +0300

    Initialize accessibility-audit

---
+++ bin/accessibility-check.js
@@ -0,0 +1,44 @@
+import pa11y from "pa11y";
+import puppeteer from "puppeteer";
+import core from "@actions/core";
+
+const navigationTimeout = 120000; // Set the navigation timeout to 120 seconds (120,000 milliseconds)
+let resultsArray = [];
+let printArray = [];
+
+async function checkLinks() {
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
+  const page = await browser.newPage();
+
+  const sitemapUrl = "https://developers.cloudflare.com/sitemap.xml";
+  await page.goto(sitemapUrl, { timeout: navigationTimeout });
+
+  const sitemapLinks = await page.$$eval("url loc", (elements) =>
+    elements.map((el) => el.textContent)
+  );
+
+  for (const link of sitemapLinks) {
+    if (!link) {
+      continue; // Skip if the link is empty
+    } else if (link.includes("/support/other-languages/")) {
+      continue; // Skip if the link is in a certain section
+    }
+
+    const testPage = await browser.newPage();
+    let result = await pa11y(link, {
+      browser,
+      page: testPage,
+      runners: ["axe", "htmlcs"],
+    });
+    console.log(result)
+    
+    await testPage.close();
+  }
+  await page.close();
+  await browser.close();
+  console.log(resultsArray)
+}
+
+checkLinks();
